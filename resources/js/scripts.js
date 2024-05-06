4ec128a75a2031720ae3ef6b79cd12df56dc3d89
Gaurav Juneja <gjuneja555@gmail.com>
2024-05-06 12:07:41 +0530

    upto skills

---
+++ resources/js/scripts.js
@@ -0,0 +1,10 @@
+// Smooth scroll for anchor links
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+});
+
+// Slider

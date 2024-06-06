f4adfe004247995a6c49b353b598e7777bcce021
Gaurav Juneja <gjuneja555@gmail.com>
2024-06-06 18:14:31 +0530

    almost

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -4,7 +4,7 @@ $('a[href*="#"]').on('click', function (e) {
 
     $('html, body').animate({
         scrollTop: $($(this).attr('href')).offset().top
-    }, 500, 'linear');
+    }, 200, 'linear');
 });
 
 // Projects
@@ -84,7 +84,5 @@ document.addEventListener("DOMContentLoaded", function () {
     carousels.forEach(carousel => {
         new bootstrap.Carousel(carousel);
     });
-
-    // Hero-Section
     
 });
